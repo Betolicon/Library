a157fc868de02f0a29b66acfea94a22da217d9b3
Betolicon <[albertlicontti@gmail.com]>
2024-09-13 18:43:01 -0400

    the project is almost done

--- script.js
+++ script.js
@@ -6,20 +6,17 @@ function Book(title, author, numberPages, read){
     this.title = title;
     this.author = author;
     this.numberPages = numberPages
-    this.read = read ? 'read' : 'not read yet'
+    this.read = read === 'yes' ? 'read' : 'not read yet'
     this.info = function(){
         return `${this.title} by ${this.author}, ${this.numberPages} pages, ${this.read}`
     }
 }
 
-function addBookToLibrary(){
-    let title = document.getElementById('title').value
-    let author = document.getElementById('author').value
-    let numberPages = document.getElementById('pages').value
-    let read = document.getElementById('read').value
+function addBookToLibrary(title, author, numberPages, read){
     const book = new Book(title, author, numberPages, read)
-
+    
     myLibrary.push(book)
+    createNewBook(book);
 }
 
 const showForm = () =>{
@@ -28,8 +25,45 @@ const showForm = () =>{
 }
 
 const closeForm = () =>{
-    addBookToLibrary();
+    let dlg = document.getElementById("form");
+    dlg.close();
+}
+
+const createNewBook = (book) =>{
+    const library = document.querySelector('.container')
+    const card = document.createElement('div')
+    card.className = 'card';
+    const title = document.createElement('h4')
+    const author = document.createElement('p')
+    const pages = document.createElement('p')
+    const read = document.createElement('p')
+    const Remove = document.createElement('button')
+    title.textContent = `${book.title}`
+    author.textContent = `Author: ${book.author}`
+    pages.textContent = `Pages: ${book.numberPages}`
+    read.textContent = `Status: ${book.read}`
+    Remove.innerHTML = 'Remove'
+    Remove.className = 'remove'
+    card.appendChild(title)
+    card.appendChild(author)
+    card.appendChild(pages)
+    card.append(read)
+    card.appendChild(Remove)
+    library.appendChild(card)
+    closeForm()
+}
+
+const SubmitForm = (e) =>{
+    let title = document.getElementById('title').value
+let author = document.getElementById('author').value
+let numberPages = document.getElementById('pages').value
+let read = document.getElementById('read').value
+    if(title.length != 0 && author.length != 0  && numberPages.length <= 0  && read.length != 0 ){
+    addBookToLibrary(title, author, numberPages, read);
+    e.preventDefault()
+    }
+
 }
 
 newBook.addEventListener('click', showForm)
-closeButton.addEventListener('click', closeForm)
+closeButton.addEventListener('click', SubmitForm)
